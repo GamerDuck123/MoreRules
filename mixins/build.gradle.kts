20193916f844520fa87251e574af7b03d60d04da
GamerDuck123 <github@quacks.media>
2025-09-02 23:41:06 -0400

    the build.gradle.kts from mixins wasnt included before

---
+++ mixins/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+    // Only needed for annotations/compile-time
+    compileOnly("org.spongepowered:mixin:0.8.5")
+
+    // For annotation processor (validates your mixins at build time)
+    annotationProcessor("org.spongepowered:mixin:0.8.5:processor")
+}
+
+neoForge {
+    neoFormVersion = "1.21.8-20250717.133445"
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
